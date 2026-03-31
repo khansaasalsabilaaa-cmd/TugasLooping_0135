cc2e1545300731e91f07c9c0138c3f68b566f6aa
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-03-31 22:15:12 +0700

    menambahakn benar salah

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -10,6 +10,8 @@ bool isPrima(int n) {
     if (n <= 1) return false;
     int i = 2;
     while (i * i <= n) {
-        
+        if (n % i == 0) return false;
+        i++;
     }
+    return true;
 }
